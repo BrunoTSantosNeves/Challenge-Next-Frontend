c6f0130fdcb2a82ab768467d1665097abe22576c
bidbibb <tonetti583@gmail.com>
2025-02-25 16:32:42 -0300

    feat: Imagens inseridas

--- capputenno/src/components/products-list.tsx
+++ capputenno/src/components/products-list.tsx
@@ -4,22 +4,25 @@ import { useProducts } from "@/context/useProducts"
 import { ProductCard } from "./product-card"
 import styled from "styled-components"
 
-const ListConteiner = styled.div`
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(256px, 4fr));
-
-    gap: 16px;
-
-
+const ListContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr); /* Três colunas iguais */
+  grid-gap: 32px;
+  max-width: 100%;
 `
 
-export function ProductsList(){
-    const { data } = useProducts()
-    return(
-        <ListConteiner>{data?.map(product => <ProductCard 
-            key={product.id}
-            title={product.name}
-            price={product.price_in_cents}
-            image={product.image_url}/>)}</ListConteiner>
-    )
+export function ProductsList() {
+  const { data } = useProducts()
+  return (
+    <ListContainer>
+      {data?.map((product) => (
+        <ProductCard
+          key={product.id}
+          title={product.name}
+          price={product.price_in_cents}
+          image={product.image_url}
+        />
+      ))}
+    </ListContainer>
+  )
 }
